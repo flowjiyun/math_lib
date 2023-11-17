29f0cfdfa852090bf0c0f8744b7fd2441070026d
flowjiyun <gyp2795@gmail.com>
2023-11-17 14:39:45 +0900

    lint: 포멧 수정

--- src/vec2.cpp
+++ src/vec2.cpp
@@ -4,88 +4,67 @@ using namespace jlm;
 
 constexpr vec2::vec2(float x, float y) : mX(x), mY(y) {}
 
-constexpr float jlm::vec2::x() const
-{
-    return mX;
-}
+constexpr float jlm::vec2::x() const { return mX; }
 
-constexpr float jlm::vec2::y() const
-{
-    return mY;
-}
+constexpr float jlm::vec2::y() const { return mY; }
 
-constexpr float jlm::vec2::operator[](uint32_t index) const
-{
+constexpr float jlm::vec2::operator[](uint32_t index) const {
     assert(index < Dimension);
     return mScalars[index];
 }
 
-constexpr float& jlm::vec2::operator[](uint32_t index)
-{
+constexpr float& jlm::vec2::operator[](uint32_t index) {
     assert(index < Dimension);
     return mScalars[index];
 }
 
-constexpr vec2 jlm::vec2::operator+(const vec2 &rhs) const
-{
+constexpr vec2 jlm::vec2::operator+(const vec2& rhs) const {
     return vec2(mX + rhs.mX, mY + rhs.mY);
 }
 
-constexpr vec2 jlm::vec2::operator-() const
-{
-    return vec2(-mX, -mY);
-}
+constexpr vec2 jlm::vec2::operator-() const { return vec2(-mX, -mY); }
 
-constexpr vec2 jlm::vec2::operator-(const vec2 &rhs) const
-{
+constexpr vec2 jlm::vec2::operator-(const vec2& rhs) const {
     return vec2(mX - rhs.mX, mY - rhs.mY);
 }
 
-constexpr vec2 jlm::vec2::operator*(float scalar) const
-{
+constexpr vec2 jlm::vec2::operator*(float scalar) const {
     return vec2(mX * scalar, mY * scalar);
 }
 
-constexpr vec2 jlm::vec2::operator*(const vec2 &rhs) const
-{
+constexpr vec2 jlm::vec2::operator*(const vec2& rhs) const {
     return vec2(mX * rhs.mX, mY * rhs.mY);
 }
 
-constexpr vec2 jlm::vec2::operator/(float scalar) const
-{
+constexpr vec2 jlm::vec2::operator/(float scalar) const {
     return vec2(mX / scalar, mY / scalar);
 }
 
-constexpr vec2 &jlm::vec2::operator+=(const vec2 &rhs)
-{
+constexpr vec2& jlm::vec2::operator+=(const vec2& rhs) {
     mX += rhs.mX;
     mY += rhs.mY;
     return *this;
 }
 
-constexpr vec2 &jlm::vec2::operator-=(const vec2 &rhs)
-{
+constexpr vec2& jlm::vec2::operator-=(const vec2& rhs) {
     mX -= rhs.mX;
     mY -= rhs.mY;
     return *this;
 }
 
-constexpr vec2 &jlm::vec2::operator*=(float scalar)
-{
+constexpr vec2& jlm::vec2::operator*=(float scalar) {
     mX *= scalar;
     mY *= scalar;
     return *this;
 }
 
-constexpr vec2 &jlm::vec2::operator/=(float scalar)
-{
+constexpr vec2& jlm::vec2::operator/=(float scalar) {
     mX /= scalar;
     mY /= scalar;
     return *this;
 }
 
-constexpr vec2 &jlm::vec2::operator*=(const vec2 &rhs)
-{
+constexpr vec2& jlm::vec2::operator*=(const vec2& rhs) {
     mX /= rhs.mX;
     mY /= rhs.mY;
     return *this;
--- src/vec2.hpp
+++ src/vec2.hpp
@@ -3,51 +3,45 @@
 #include <array>
 #include <cassert>
 
-namespace jlm
-{
-    class vec2
-    {
-        public:
-            // constructor
-            constexpr vec2() = default;
-            explicit constexpr vec2(float x, float y);
+namespace jlm {
+class vec2 {
+  public:
+    // constructor
+    constexpr vec2() = default;
+    explicit constexpr vec2(float x, float y);
 
-            // copy constructor & assignment operator
-            constexpr vec2(const vec2& other) = default;
-            constexpr vec2& operator=(const vec2& other) = default;
+    // copy constructor & assignment operator
+    constexpr vec2(const vec2& other) = default;
+    constexpr vec2& operator=(const vec2& other) = default;
 
-            // getter
-            constexpr float x() const;
-            constexpr float y() const;
+    // getter
+    constexpr float x() const;
+    constexpr float y() const;
 
-            // accessor
-            constexpr float operator[](uint32_t index) const;
-            constexpr float& operator[](uint32_t index);
+    // accessor
+    constexpr float operator[](uint32_t index) const;
+    constexpr float& operator[](uint32_t index);
 
-            //operator
-            constexpr vec2 operator+(const vec2& rhs) const;
-            constexpr vec2 operator-() const;
-            constexpr vec2 operator-(const vec2& rhs) const;
-            constexpr vec2 operator*(float scalar) const;
-            constexpr vec2 operator*(const vec2& rhs) const;
-            constexpr vec2 operator/(float scalar) const;
-            constexpr vec2& operator+=(const vec2& rhs);
-            constexpr vec2& operator-=(const vec2& rhs);
-            constexpr vec2& operator*=(float scalar);
-            constexpr vec2& operator/=(float scalar);
-            constexpr vec2& operator*=(const vec2& rhs);
+    // operator
+    constexpr vec2 operator+(const vec2& rhs) const;
+    constexpr vec2 operator-() const;
+    constexpr vec2 operator-(const vec2& rhs) const;
+    constexpr vec2 operator*(float scalar) const;
+    constexpr vec2 operator*(const vec2& rhs) const;
+    constexpr vec2 operator/(float scalar) const;
+    constexpr vec2& operator+=(const vec2& rhs);
+    constexpr vec2& operator-=(const vec2& rhs);
+    constexpr vec2& operator*=(float scalar);
+    constexpr vec2& operator/=(float scalar);
+    constexpr vec2& operator*=(const vec2& rhs);
 
-
-        private:
-            static constexpr uint32_t Dimension {2};
-            union
-            {
-                struct
-                {
-                    float mX, mY;
-                };
-                std::array<float, Dimension> mScalars {0.0f, 0.0f};
-            };
-            
+  private:
+    static constexpr uint32_t Dimension{2};
+    union {
+        struct {
+            float mX, mY;
+        };
+        std::array<float, Dimension> mScalars{0.0f, 0.0f};
     };
-}
+};
+} // namespace jlm
