61f9e9e8b9723e4890c3bc5cef1640206821d36d
flowjiyun <gyp2795@gmail.com>
2023-11-21 12:22:27 +0900

    fix: constexpr 사용을 위한 헤더 파일내 함수 정의

--- src/vec2.hpp
+++ src/vec2.hpp
@@ -17,6 +17,8 @@ class vec2 {
     // getter
     constexpr float x() const;
     constexpr float y() const;
+    const float* ptr() const;
+    float* ptr();
 
     // accessor
     constexpr float operator[](uint32_t index) const;
@@ -44,4 +46,75 @@ class vec2 {
         std::array<float, Dimension> mScalars{0.0f, 0.0f};
     };
 };
+
+constexpr vec2::vec2(float x, float y) : mX(x), mY(y) {}
+
+constexpr float vec2::x() const { return mX; }
+
+constexpr float vec2::y() const { return mY; }
+
+const float* vec2::ptr() const { return &mX; }
+float* vec2::ptr() { return &mX; }
+
+constexpr float vec2::operator[](uint32_t index) const {
+    assert(index < Dimension);
+    return mScalars[index];
+}
+
+constexpr float& vec2::operator[](uint32_t index) {
+    assert(index < Dimension);
+    return mScalars[index];
+}
+
+constexpr vec2 vec2::operator+(const vec2& rhs) const {
+    return vec2(mX + rhs.mX, mY + rhs.mY);
+}
+
+constexpr vec2 vec2::operator-() const { return vec2(-mX, -mY); }
+
+constexpr vec2 vec2::operator-(const vec2& rhs) const {
+    return vec2(mX - rhs.mX, mY - rhs.mY);
+}
+
+constexpr vec2 vec2::operator*(float scalar) const {
+    return vec2(mX * scalar, mY * scalar);
+}
+
+constexpr vec2 vec2::operator*(const vec2& rhs) const {
+    return vec2(mX * rhs.mX, mY * rhs.mY);
+}
+
+constexpr vec2 vec2::operator/(float scalar) const {
+    return vec2(mX / scalar, mY / scalar);
+}
+
+constexpr vec2& vec2::operator+=(const vec2& rhs) {
+    mX += rhs.mX;
+    mY += rhs.mY;
+    return *this;
+}
+
+constexpr vec2& vec2::operator-=(const vec2& rhs) {
+    mX -= rhs.mX;
+    mY -= rhs.mY;
+    return *this;
+}
+
+constexpr vec2& vec2::operator*=(float scalar) {
+    mX *= scalar;
+    mY *= scalar;
+    return *this;
+}
+
+constexpr vec2& vec2::operator/=(float scalar) {
+    mX /= scalar;
+    mY /= scalar;
+    return *this;
+}
+
+constexpr vec2& vec2::operator*=(const vec2& rhs) {
+    mX /= rhs.mX;
+    mY /= rhs.mY;
+    return *this;
+}
 } // namespace jlm
