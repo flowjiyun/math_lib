5886e71da575cadc19cc69c0ec60bcdfcd7b62ef
flowjiyun <gyp2795@gmail.com>
2023-11-21 12:25:25 +0900

    feat: test 시스템 구축 - makefile 작성 및 테스트 일부 작성

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,6 +1,17 @@
+#include "../src/utils.hpp"
 #include "../src/vec2.hpp"
+#include <iostream>
 
 int main() {
-    
+    // int Error = 0;
+
+    jlm::vec2 a = jlm::vec2(1.0f, 1.0f);
+    jlm::vec2 b = jlm::vec2(2.0f, 2.0f);
+    std::cout << a.x() << " " << a.y() << std::endl;
+
+    jlm::vec2 c = a + b;
+    std::cout << c.x() << " " << c.y() << std::endl;
+
+    std::cout << *jlm::ValuePtr(c) << std::endl;
     return 0;
 }
