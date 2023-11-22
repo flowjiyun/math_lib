6c3948fd7ef6aab70ed171700e562cc8d4628d57
flowjiyun <gyp2795@gmail.com>
2023-11-22 14:29:31 +0900

    feat: Todo 구현 필요 항목 추가

--- src/utils.hpp
+++ src/utils.hpp
@@ -7,4 +7,16 @@ namespace jlm {
 const float* ValuePtr(const vec2& v) { return v.ptr(); }
 
 float* ValuePtr(vec2& v) { return v.ptr(); }
+
+// Todo: perspective
+// Todo: translate
+// Todo: scale
+// Todo: rotate
+// Todo: radians
+// Todo: lookat
+// Todo: dot
+// Todo: cross
+// Todo: normalize
+// Todo: clamp
+
 } // namespace jlm
