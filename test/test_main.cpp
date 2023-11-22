f65569a60060b8e3f26005c2681e0e299d6a7993
flowjiyun <gyp2795@gmail.com>
2023-11-22 14:29:11 +0900

    test: mat2 테스트 케이스 추가
    - 곱연산 및 동등 비교 연산 케이스

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,10 +1,11 @@
 #include "../src/mat2.hpp"
 #include "../src/utils.hpp"
 #include "../src/vec2.hpp"
+#include <cassert>
 #include <iostream>
 
 int main() {
-    // int Error = 0;
+    int Error = 0;
 
     jlm::vec2 a = jlm::vec2(1.0f, 1.0f);
     jlm::vec2 b = jlm::vec2(2.0f, 2.0f);
@@ -18,6 +19,33 @@ int main() {
     std::cout << a.Size() << std::endl;
 
     std::cout << "Mat" << std::endl;
-    jlm::mat2 a = jlm::mat2();
-    return 0;
+    // check mat * vec
+    {
+        jlm::mat2 i = jlm::mat2(jlm::vec2::UnitX, jlm::vec2::UnitY);
+        jlm::vec2 v = jlm::vec2(3.0f, 1.0f);
+        jlm::vec2 d = i * v;
+        if (v != d) {
+            Error++;
+        }
+    }
+    // check mat * mat
+    {
+        jlm::mat2 a = jlm::mat2(jlm::vec2(1.0f, 3.0f), jlm::vec2(2.0f, 4.0f));
+        jlm::mat2 ans =
+            jlm::mat2(jlm::vec2(7.0f, 15.0f), jlm::vec2(10.0f, 22.0f));
+        if (a * a != ans) {
+            Error++;
+        }
+    }
+    // check mat * scalar
+    {
+        jlm::mat2 a = jlm::mat2(jlm::vec2(1.0f, 3.0f), jlm::vec2(2.0f, 4.0f));
+        jlm::mat2 ans = jlm::mat2(jlm::vec2(2.0f, 6.0f), jlm::vec2(4.0f, 8.0f));
+        if (a * 2 != ans) {
+            Error++;
+        }
+        assert(a * 2 == ans);
+    }
+
+    return Error;
 }
